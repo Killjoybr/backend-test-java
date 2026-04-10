185cd259ae447dff6b3c3422825c12129e470abd
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2026-04-10 01:07:14 -0300

    init spring app
    Initial Spring initializ code

---
+++ src/main/java/br/com/ruandavid/estacionamentofcamara/EstacionamentofcamaraApplication.java
@@ -0,0 +1,13 @@
+package br.com.ruandavid.estacionamentofcamara;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentofcamaraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentofcamaraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ruandavid/estacionamentofcamara/EstacionamentofcamaraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ruandavid.estacionamentofcamara;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionamentofcamaraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
